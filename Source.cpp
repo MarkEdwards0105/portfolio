cbeb8d1aae5fe670fadd875634b8bb5f5557f4a3
MarkEdwards0105 <83669653+MarkEdwards0105@users.noreply.github.com>
2021-07-29 13:05:07 +0900

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+//Write a program the produces the following output
+//*************************************************
+//* Programming Assignment 1                      *
+//* Computer Programming 1                        *
+//*				Author: ???                       *
+//* Due Date: Thursday, Jan. 24                   *
+//*************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "******************************" << endl;
+	cout << "* Programming Assignment 1   *" << endl;
+	cout << "* Computer Programming 1     *" << endl;
+	cout << "* Author: Prog Let's Plays   *" << endl;
+	cout << "* Due Date: Thursday, Jan.24 *" << endl;
+	cout << "******************************" << endl;
+
+	return 0;
+}
