8a107af6c56f71e8d562370b4886431cb6607a5d
MarkEdwards0105 <83669653+MarkEdwards0105@users.noreply.github.com>
2021-07-29 13:17:14 +0900

    Update ProgrammingExercise2-1.cpp

--- ProgrammingExercise2-1.cpp
+++ ProgrammingExercise2-1.cpp
@@ -17,7 +17,7 @@ int main()
 	cout << "******************************" << endl;
 	cout << "* Programming Assignment 1   *" << endl;
 	cout << "* Computer Programming 1     *" << endl;
-	cout << "* Author: Mark Edwards   *" << endl;
+	cout << "* Author: Mark Edwards       *" << endl;
 	cout << "* Due Date: Thursday, Jan.24 *" << endl;
 	cout << "******************************" << endl;
 
